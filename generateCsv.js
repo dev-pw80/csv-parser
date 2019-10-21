0eaf62693735a7a4efcd7f5846ede718df0898aa
dev-pw80 <patrick.weiss80@gmail.com>
2019-10-21 17:04:54 +0200

    create csv from search

--- generateCsv.js
+++ generateCsv.js
@@ -4,6 +4,8 @@ const Aqmpoint = require('./Aqmpoint');
 
 require('dotenv').config();
 
+const fs = require('fs')
+
 mongoose.connect(process.env.MONGOURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -13,8 +15,15 @@ mongoose.connect(process.env.MONGOURL, {
 
 (async () => {
 
-    const foundPost = await Aqmpoint.find();
-    console.log(foundPost);
-
+    const foundPost = await Aqmpoint.find({'aqm.noxevent':{$gte: 50}});
+    
+    let CsvText = 'r3000, r5000, noxevent, r2000, coevent, system_time, gps_time, latitude, longitude\n' ;
+    
+    foundPost.map(point => {
+        CsvText += `${point.aqm.r3000}, ${point.aqm.r5000}, ${point.aqm.noxevent}, ${point.aqm.r2000}, ${point.aqm.coevent}, ${point.gps.system_time}, ${point.gps.gps_time}, ${point.gps.latitude}, ${point.gps.longitude}\n`
+    });
+    
+    await fs.writeFileSync(process.env.DATALINK, CsvText)
+    console.log(foundPost, CsvText);
     process.exit();
 })();
